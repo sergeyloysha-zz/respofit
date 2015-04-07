e6fbf22fbf8d620f89e812c4266032e0ed92db72
Dave Trent <imdavetrent@gmail.com>
2015-04-07 23:47:59 +0300

    update

--- respofit/src/js/app.js
+++ respofit/src/js/app.js
@@ -5,4 +5,13 @@ $(function() {
     	$('.head__search-input').removeClass('hide').focus();
     	console.log('open search');
     });
+
+    $('.header__menu-link_menu').on('click', function(e){
+    	if($(this).hasClass('active')) {
+    		$(this).removeClass('active');
+    	}
+    	else {
+    		$(this).addClass('active');
+    	}
+    })
 });
